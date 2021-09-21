a682afa0581de4eb13297a565bc0d4fe115b4afc
kelsonfox <kelsonbatista@yahoo.co.uk>
2021-09-20 22:27:35 -0300

    Direction and limits

--- js/main.js
+++ js/main.js
@@ -1,37 +1,51 @@
 let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
-let bloco = 32;
+let box = 32;
 let snake = [];
 snake[0] = {
-    x: 8 * bloco,
-    y: 8 * bloco
+    x: 8 * box,
+    y: 8 * box
 }
 let direction = "right";
 
 function criarBG() {
     context.fillStyle = "lightgreen";
-    context.fillRect(0,0,16 * bloco, 16 * bloco);
+    context.fillRect(0,0,16 * box, 16 * box);
 }
 
 function criarCobrinha() {
     for(i=0; i < snake.length; i++){
         context.fillStyle = "green";
-        context.fillRect(snake[i].x, snake[i].y, bloco, bloco);
+        context.fillRect(snake[i].x, snake[i].y, box, box);
     }
 }
 
+document.addEventListener("keydown", update);
+
+function update(event) {
+    if(event.keyCode == 37 && direction != "right") direction = "left";
+    if(event.keyCode == 38 && direction != "down") direction = "up";
+    if(event.keyCode == 39 && direction != "left") direction = "right";
+    if(event.keYCode == 40 && direction != "up") direction = "down";
+}
+
 function iniciarJogo() {
-    
+    // se ultrapassa a tela, ela bate na parede
+    if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
+    if(snake[0].x < 0  && direction  == "left") snake[0].x = 16 * box;
+    if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
+    if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+
     criarBG();
     criarCobrinha();
 
     let snakex = snake[0].x;
     let snakey = snake[0].y;
 
-    if(direction == "right") snakex += bloco; //adiciona um bloco 
-    if(direction == "left") snakex -= bloco; //retira um bloco
-    if(dicrection == "up") snakey -= bloco;
-    if(direction == "down") snakey += bloco;
+    if(direction == "right") snakex += box; //adiciona um box
+    if(direction == "left") snakex -= box; //retira um box
+    if(direction == "up") snakey -= box;
+    if(direction == "down") snakey += box;
 
     snake.pop();
 
@@ -41,7 +55,7 @@ function iniciarJogo() {
     }
 
     snake.unshift(newHead);
-    
+
 }
 
 let jogo = setInterval(iniciarJogo, 100); //a cada 100ms a tela é renovada e atualiza o jogo
