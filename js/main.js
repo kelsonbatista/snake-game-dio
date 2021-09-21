e38d09e800b8076f21bb0d66ffa9fd31bf772e05
kelsonfox <kelsonbatista@yahoo.co.uk>
2021-09-20 21:46:09 -0300

    Snake created

--- js/main.js
+++ js/main.js
@@ -1,10 +1,23 @@
 let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
 let bloco = 32;
+let snake = [];
+snake[0] = {
+    x: 8 * bloco,
+    y: 8 * bloco
+}
 
 function criarBG() {
     context.fillStyle = "lightgreen";
     context.fillRect(0,0,16 * bloco, 16 * bloco);
 }
 
+function criarCobrinha() {
+    for(i=0; i < snake.length; i++){
+        context.fillStyle = "green";
+        context.fillRect(snake[i].x, snake[i].y, bloco, bloco);
+    }
+}
+
 criarBG();
+criarCobrinha();
