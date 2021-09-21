4e8a4c33dabe9054dcbc4efb3ddfee20055bbd74
kelsonfox <kelsonbatista@yahoo.co.uk>
2021-09-20 23:45:00 -0300

    Final layout

--- js/main.js
+++ js/main.js
@@ -35,15 +35,23 @@ function update(event) {
     if(event.keyCode == 37 && direction != "right") direction = "left";
     if(event.keyCode == 38 && direction != "down") direction = "up";
     if(event.keyCode == 39 && direction != "left") direction = "right";
-    if(event.keYCode == 40 && direction != "up") direction = "down";
+    if(event.keyCode == 40 && direction != "up") direction = "down";
 }
 
 function iniciarJogo() {
+
     // se ultrapassa a tela, ela bate na parede
     if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
     if(snake[0].x < 0  && direction  == "left") snake[0].x = 16 * box;
     if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
     if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+
+    for(i=1; i<snake.length; i++) {
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y) {
+            clearInterval(jogo);
+            alert("Game Over!!!");
+        }
+    }
 
     criarBG();
     criarCobrinha();
@@ -73,5 +81,5 @@ function iniciarJogo() {
 
 }
 
-let jogo = setInterval(iniciarJogo, 100); //a cada 100ms a tela é renovada e atualiza o jogo
+let jogo = setInterval(iniciarJogo, 100); //a cada 100ms a tela é renovada e atualiza o jogo - velocidade do jogo
 
