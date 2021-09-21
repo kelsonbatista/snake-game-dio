d9c054ae4f1f19dc0a39b814a2ff5f99aa0701cc
kelsonfox <kelsonbatista@yahoo.co.uk>
2021-09-20 21:41:07 -0300

    Parte 1A

---
+++ js/main.js
@@ -0,0 +1,10 @@
+let canvas = document.getElementById("snake");
+let context = canvas.getContext("2d");
+let bloco = 32;
+
+function criarBG() {
+    context.fillStyle = "lightgreen";
+    context.fillRect(0,0,16 * bloco, 16 * bloco);
+}
+
+criarBG();
